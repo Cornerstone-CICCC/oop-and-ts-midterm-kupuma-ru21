d389fa5e32c3fb04be18b3f75b97c9fdb9fb19d9
kupuma-ru21 <tech.kupumaru@gmail.com>
2024-09-26 15:19:31 -0700

    chore: wip

--- src/components/App/products/functions/dom/createProductCard.js
+++ src/components/App/products/functions/dom/createProductCard.js
@@ -1,13 +1,8 @@
 export const createProductCard = ({ productId }) => {
-  const productCard = document.createElement("a");
+  const productCard = document.createElement("div");
   productCard.style.display = "flex";
   productCard.style.gap = "20px";
   productCard.style.outline = "1px solid gray";
-
-  productCard.onclick = () => {
-    console.log("productCard clicked");
-    history.pushState({}, "", productId);
-  };
 
   return { productCard };
 };
